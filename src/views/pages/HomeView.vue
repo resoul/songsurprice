342ccc537b6e0060d7d462da20e53de3137a66bc
resoul <resoul.ua@icloud.com>
2024-11-20 12:27:46 +0200

    update to v2.1.3

--- src/views/pages/HomeView.vue
+++ src/views/pages/HomeView.vue
@@ -1,15 +1,27 @@
 <script>
 import Footer from "@/views/components/Footer.vue";
 import Header from "@/views/components/Header.vue";
+import CreateTrack from "@/views/pages/home/CreateTrack.vue";
+import Features from "@/views/pages/home/Features.vue";
+import HowItWork from "@/views/pages/home/HowItWork.vue";
+import Testimonials from "@/views/pages/home/Testimonials.vue";
 export default {
   components: {
     'ui-header': Header,
-    'ui-footer': Footer
+    'ui-footer': Footer,
+    'ui-features': Features,
+    'ui-testimonials': Testimonials,
+    'ui-order': CreateTrack,
+    'ui-how-it-work': HowItWork
   }
 }
 </script>
 
 <template>
   <ui-header class="relative z-50" />
+  <ui-features class="container relative px-4 mx-auto overflow-hidden max-w-7xl mt-8 sm:mt-20" />
+  <ui-how-it-work class="container relative px-4 mx-auto overflow-hidden max-w-7xl mt-8 sm:mt-20" />
+  <ui-order class="-mt-12 mb-8 sm:mb-20" />
+  <ui-testimonials class="relative overflow-hidden mb-20" />
   <ui-footer class="relative px-3 py-24 -mt-10 sm:px-0" />
 </template>
